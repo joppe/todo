7be7ff1791b8c062f52850b5c1a585a8c4c9e329
joppe <joppe@apestaartje.info>
2025-05-05 22:08:45 +0200

    code formatting

--- src/types/MergeTypes.ts
+++ src/types/MergeTypes.ts
@@ -1,7 +1,5 @@
-// biome-ignore lint/complexity/noBannedTypes: an empty object is needed
 export type MergeTypes<Types extends unknown[], Result = {}> = Types extends [
-	infer Head,
-	...infer Tail,
-]
-	? MergeTypes<Tail, Head & Omit<Result, keyof Head>>
-	: Result;
+  infer Head,
+  ...infer Tail,
+] ? MergeTypes<Tail, Head & Omit<Result, keyof Head>>
+  : Result;
